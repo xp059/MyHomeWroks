adfd64f23e3038aabf07f70d09a920df6afeaeca
Aleksandr <sanpetro00@mail.ru>
2022-10-04 16:49:10 +0300

    Thrid work

--- MyWork/Program.cs
+++ MyWork/Program.cs
@@ -37,3 +37,19 @@ if (numC > max)
 
 Console.WriteLine(max);
 */
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+/*
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 1)
+{
+    Console.WriteLine($"Число {num} нечетное");
+}
+else
+{
+    Console.WriteLine($"Число {num} четное");
+}
+return num;
+*/
