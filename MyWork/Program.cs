8683645eb4e0660d259987925a1921bf7a9f4d05
Aleksandr <sanpetro00@mail.ru>
2022-10-04 14:08:07 +0300

    Second work

--- MyWork/Program.cs
+++ MyWork/Program.cs
@@ -15,3 +15,25 @@ else
     Console.WriteLine($"Второе число {numB} больше чем первое {numA}");
 }
 */
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+/*
+Console.Write("Введите 3 числа: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+int numB = Convert.ToInt32(Console.ReadLine());
+int numC = Convert.ToInt32(Console.ReadLine());
+
+int max = numA;
+
+if (numB > max)
+{
+    max = numB;
+}
+
+if (numC > max)
+{
+    max = numC;
+}
+
+Console.WriteLine(max);
+*/
